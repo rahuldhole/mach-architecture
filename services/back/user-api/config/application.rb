979feee9cc3b2ec67b4bbfc0ca57f9274064cde9
rahuldhole <rdhole95@gmail.com>
2024-03-26 22:26:58 +0000

    create docker-compose with postgres and rails

--- services/back/user-api/config/application.rb
+++ services/back/user-api/config/application.rb
@@ -24,6 +24,10 @@ module UserApi
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
+    # ALLOWED_HOSTS
+    allowed_hosts = ENV['ALLOWED_HOSTS']&.split(',') || ['0.0.0.0']
+    config.hosts.concat(allowed_hosts) if allowed_hosts.present?
+
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
