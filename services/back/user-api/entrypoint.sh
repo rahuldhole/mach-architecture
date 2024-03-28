5d6027e50bbdcfa8c58357d466923c52d5085f48
rahuldhole <rdhole95@gmail.com>
2024-03-28 11:10:41 +0000

    verify db in entrypoint
    PORT env variable can be @override in app so use RAILS_PORT in .env to assign new port

--- services/back/user-api/entrypoint.sh
+++ services/back/user-api/entrypoint.sh
@@ -5,21 +5,24 @@ set -e
 start_server() {
   local port="$1"
   local rails_env="$2"
+  local database_host="$3"
+  local database_port="$4"
 
-  if [ "$rails_env" = 'production' ]; then
-    echo "Starting Rails server in production environment..."
-  else
-    echo "Installing bundle for development/test environment..."
+  if [ "$rails_env" != 'production' ]; then
+    echo "Installing bundle for $rails_env environment..."
     bundle install
-    echo "Starting Rails server in development/test environment..."
   fi
 
-  echo "Waiting for postgres to be ready..."
-  while ! nc -z postgres 5432; do
+  echo "Waiting for $database_host to be ready..."
+  while ! nc -z "$database_host" $database_port; do
     sleep 1
   done
+  echo "$database_host is ready..."
 
-  echo "Postgres is ready. Starting Rails server..."
+  echo "Creating and migrating database in $rails_env environment..."
+  bin/rails db:create && bin/rails db:migrate
+  
+  echo "Starting Rails server in $rails_env environment..."
   bundle exec rails server -b 0.0.0.0 -p "$port"
 }
 
@@ -27,9 +30,13 @@ start_server() {
 env_defaults() {
   PORT=${PORT:-3000}
   RAILS_ENV=${RAILS_ENV:-"development"}
+  DATABASE_HOST=${DATABASE_HOST:-"postgres"}
+  DATABASE_PORT=${DATABASE_PORT:-"5432"}
+
+  echo "RAILS env $RAILS_ENV port $PORT | DB host $DATABASE_HOST port $DATABASE_PORT"
 }
 
 #! |\/| _ o _ 
 #! |  |(_||| ) execution starts here
 env_defaults  # Set default values
-start_server "$PORT" "$RAILS_ENV"  # Start the Rails server
+start_server "$PORT" "$RAILS_ENV" "$DATABASE_HOST" "$DATABASE_PORT"
